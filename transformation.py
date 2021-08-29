794c665182a511236ada0bbbc3d93a85361cae7c
HYEONG <heeyoungmy@yonsei.ac.kr>
2021-08-29 14:40:57 +0000

    transformation cafe

---
+++ transformation.py
@@ -0,0 +1,62 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+from torchvision import transforms
+from torchvision.transforms import Resize, ToTensor, Normalize
+
+def get_transform(t_name):
+    
+    if t_name == 'water':
+        t = A.Compose(
+        [
+            A.Resize(224, 224),
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+            A.Normalize(mean=0, std=1),
+            ToTensorV2(),
+        ])
+        return t
+
+    elif t_name =='americano':
+        t = A.Compose(
+        [
+            A.CenterCrop(height=400, width=384),
+            A.Resize(224, 224),
+            A.Normalize(mean=0, std=1),
+            ToTensorV2(),
+        ])
+        return t
+
+    elif t_name == 'latte':
+        t = A.Compose(
+        [
+            A.CenterCrop(height=400, width=384),
+            A.Resize(224, 224),
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+            A.Normalize(mean=0, std=1),
+            ToTensorV2(),
+        ])
+        return t
+
+    # inference
+
+    elif t_name == 'water_infer':
+        t = A.Compose(
+        [
+            A.Resize(224, 224),
+            A.Normalize(mean=0, std=1),
+            ToTensorV2(),
+        ])
+        return t
+
+    elif t_name == 'americano_infer' or t_name == 'latte_infer':
+        t = A.Compose(
+        [
+            A.CenterCrop(height=400, width=384),
+            A.Resize(224, 224),
+            A.Normalize(mean=0, std=1),
+            ToTensorV2(),
+        ])
+        return t
+    
