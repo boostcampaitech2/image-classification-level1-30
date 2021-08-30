2403a36f06174f897002ae3a1e156d5a32a5c319
jskwak98 <jskwak98@naver.com>
2021-08-30 05:30:25 +0000

    Modified main.py and train.py in order to make validation process

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import torchvision
 import logging
 
 
-def train(model, train_dataloader, optimizer, criterion, epoch, device, min_loss, writer, global_step):
+def train(model, train_dataloader, validation_dataloader, optimizer, criterion, epoch, device, min_val_loss, writer, global_step):
     # For computing average of loss, metric, accuracy
     loss_list = []
     metric_list = []
@@ -46,16 +46,47 @@ def train(model, train_dataloader, optimizer, criterion, epoch, device, min_loss
         optimizer.step()
     
 
-    # TODO: Save in logfile
+    # Validation
+    model.eval()
+
+    val_loss_list = []
+    val_metric_list = []
+    val_acc_list = []
+    
+    for val_batch_idx, (val_data, val_target) in enumerate(tqdm(validation_dataloader)):
+        val_x, val_target = val_data.to(device), val_target.to(device)
+
+        # Forward
+        val_y_pred = model(val_x)
+        
+        # Computing Loss
+        val_train_loss, val_train_metric = criterion.loss_fn(val_target, val_y_pred)
+
+        val_max_idx = torch.argmax(val_y_pred, dim=-1)
+
+        # For each epoch, if batch_idx is multiple of 5, write five image dataset in tfboard
+        if val_batch_idx % 10 == 0:
+            writer.add_images(f'step:{global_step}\tval_y_pred:[{val_max_idx[0:5]}\tval_y_atcual:[{val_target[0:5]}]]', val_data[0:5,:,:,:], global_step=global_step)
+
+        val_loss_list.append(val_train_loss.detach().cpu().numpy())
+        val_metric_list.append(val_train_metric)
+        val_acc = sum(val_target.detach().cpu().numpy()==val_max_idx.detach().cpu().numpy())/len(val_data)
+        val_acc_list.append(val_acc)
+
+
+
     avg_acc = sum(acc_list)/len(train_dataloader)
     avg_metric = sum(metric_list)/len(train_dataloader)
     avg_loss = sum(loss_list)/len(train_dataloader)
-    print(f"[Epoch {epoch}] Train Accuracy: {avg_acc}\t F1 Score: {avg_metric}\t Loss: {avg_loss}")
+    val_avg_acc = sum(val_acc_list)/len(validation_dataloader)
+    val_avg_metric = sum(val_metric_list)/len(validation_dataloader)
+    val_avg_loss = sum(val_loss_list)/len(validation_dataloader)
+    print(f"[Epoch {epoch}] Train Accuracy: {avg_acc}\t F1 Score: {avg_metric}\t Loss: {avg_loss}\nValidation Accuracy: {val_avg_acc}\t Validation F1 Score: {val_avg_metric}\t Validation Loss: {val_avg_loss}")
 
     logging.basicConfig(filename='Performance.log',level=logging.INFO)
-    logging.info(f"[Epoch {epoch}] Train Accuracy: {avg_acc}\t F1 Score: {avg_metric}\t Loss: {avg_loss}")
+    logging.info(f"[Epoch {epoch}] Train Accuracy: {avg_acc}\t F1 Score: {avg_metric}\t Loss: {avg_loss}\nValidation Accuracy: {val_avg_acc}\t Validation F1 Score: {val_avg_metric}\t Validation Loss: {val_avg_loss}\n")
 
 
-    if min_loss > avg_loss:
-        torch.save(model, f'./checkpoints/model.pt')
-    return min(min_loss, avg_loss), avg_acc, avg_metric, avg_loss
+    if min_val_loss > val_avg_loss:
+        torch.save(model, f'./checkpoints/Epoch{epoch}_val_F1{val_avg_metric:.3f}_val_acc{val_avg_acc:4.2%}model.pt')
+    return min(min_val_loss, val_avg_loss), avg_acc, avg_metric, avg_loss, val_avg_acc, val_avg_metric, val_avg_loss
